a15d4ce9f8b5fac12ba2bfc38f488c2c5ca48a30
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-23 22:25:55 +0530

    Move server entry file to backend root directory

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('server is running successfully');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
