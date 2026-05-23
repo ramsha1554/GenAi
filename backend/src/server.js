d0758e8cb9b11d8f5589b3b80299f52e339cadcf
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-23 22:34:07 +0530

    Configure Express middleware and CORS setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 
 
 const app = express();
+
+
+app.use (express.json());
+app.use (cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+
 
 app.get('/', (req, res) => {
   res.send('server is running successfully');
@@ -12,3 +26,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
