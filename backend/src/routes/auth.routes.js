dae61526de1eda31c1641847d6b396e40abde8cb
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-24 14:41:57 +0530

    Protect authentication routes using middleware

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,41 @@
-import {Router} from 'express';
+import { Router } from "express";
 
-
-
+import {
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  getMeController
+} from "../controllers/auth.controller.js";
+import { authUser } from "../middleware/auth.middleware.js";   
 const authRouter = Router();
+
+
+
+/**
+ * @route POST /api/auth/register
+ * @description Register new user
+ * @access Public
+ */
+authRouter.post("/register", registerUserController);
+
+
+
+/**
+ * @route POST /api/auth/login
+ * @description Login user
+ * @access Public
+ */
+authRouter.post("/login", loginUserController);
+
+
+
+/**
+ * @route GET /api/auth/logout
+ * @description Logout user
+ * @access Public
+ */
+authRouter.get("/logout", logoutUserController);
+
+
 
 export default authRouter;
