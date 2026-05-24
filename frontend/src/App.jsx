c7b522036651dce16637d2d4d2f5bb526809f5bc
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-24 20:02:01 +0530

    Add protected interview workflow routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+
 import ProtectedRoute from "./routes/ProtectedRoute";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
+import CreateInterview from "./pages/CreateInterview";
+import InterviewDetails from "./pages/InterviewDetails";
+import Reports from "./pages/Reports";
+
+
+
 function App() {
 
   return (
+
     <Routes>
 
       <Route path="/" element={<Home />} />
@@ -16,16 +25,52 @@ function App() {
 
       <Route path="/register" element={<Register />} />
 
+
+
       <Route
-  path="/dashboard"
-  element={
-    <ProtectedRoute>
-      <Dashboard />
-    </ProtectedRoute>
-  }
-/>
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+
+
+      <Route
+        path="/create-interview"
+        element={
+          <ProtectedRoute>
+            <CreateInterview />
+          </ProtectedRoute>
+        }
+      />
+
+
+
+      <Route
+        path="/reports"
+        element={
+          <ProtectedRoute>
+            <Reports />
+          </ProtectedRoute>
+        }
+      />
+
+
+
+      <Route
+        path="/interview/:id"
+        element={
+          <ProtectedRoute>
+            <InterviewDetails />
+          </ProtectedRoute>
+        }
+      />
 
     </Routes>
+
   );
 
 }
