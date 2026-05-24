928661365e74fba5a9d540b691fc86990b533d9b
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-24 15:11:33 +0530

    Implement global authentication context provider

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,26 @@
-
-
-
-
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+
+  const [user, setUser] = useState(null);
+
+  return (
+
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser
+      }}
+    >
+
+      {children}
+
+    </AuthContext.Provider>
+
+  );
+
+}
+
+export default AuthProvider;
