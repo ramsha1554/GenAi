ab6375f51c49f8cb5035c8e585f0684b9787be22
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-24 15:25:26 +0530

    Implement frontend registration form with backend integration

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,91 @@
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
 import AuthLayout from "../layouts/AuthLayout";
 
+import axiosInstance from "../api/axios";
+
 function Register() {
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+
+
+  const handleRegister = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      await axiosInstance.post(
+        "/auth/register",
+        {
+          username,
+          email,
+          password
+        }
+      );
+
+      // redirect login page
+      navigate("/login");
+
+    } catch (error) {
+
+      console.log(error.response.data);
+
+    }
+
+  };
+
+
 
   return (
 
     <AuthLayout>
 
-      <h1>Register Page</h1>
+      <h1>Register</h1>
+
+      <form onSubmit={handleRegister}>
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <br />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+
+        <button type="submit">
+          Register
+        </button>
+
+      </form>
 
     </AuthLayout>
 
